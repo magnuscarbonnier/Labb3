5805cfb7b537acea57fdaf13f1e8af9a5b115dda
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-05-19 17:52:18 +0200

    add api project

---
+++ ProductService/ProductsContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace ProductService
+{
+  
+        public class ProductsContext : DbContext
+        {
+            public ProductsContext(DbContextOptions<ProductsContext> options)
+               : base(options)
+            {
+            }
+
+            public DbSet<Product> Products { get; set; }
+
+        }
+
+    
+}
