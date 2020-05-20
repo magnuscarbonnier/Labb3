b442deaad7528579e5eb064f8f15d33869cd879b
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-05-20 12:06:50 +0200

    add ordermodel
    add context
    add ordermodel

---
+++ OrdersAPI/Data/OrdersContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdersAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersAPI.Data
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext(DbContextOptions<OrdersContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
