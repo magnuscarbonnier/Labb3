d2579ad8950c0cbabd19eed56f78e276db13a7df
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-05-24 22:05:26 +0200

    add tests

---
+++ ProductsAPI.Tests/ProductsServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProductsAPI.Tests
+{
+    public class ProductsServiceTests
+    {
+        [Fact]
+        public async Task GetAll_ReturnOK()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/products");
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+        [Fact]
+        public async Task GetProductWithEmptyGuidReturnsBadRequest()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                Guid guid = Guid.NewGuid();
+                var response = await client.GetAsync($"/api/products/{guid}");
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+        [Fact]
+        public async Task GetSingleNotAGuid_Return400()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                Guid g = Guid.NewGuid();
+                var response = await client.GetAsync($"/api/product/123");
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+    }
+}
---
+++ ProductsAPI.Tests/TestClientProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProductService;
+
+namespace ProductsAPI.Tests
+{
+    public class TestClientProvider : IDisposable
+    {
+        public TestServer Server { get; private set; }
+        public HttpClient Client { get; private set; }
+        public TestClientProvider()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            WebHostBuilder webHostBuilder = new WebHostBuilder();
+            webHostBuilder.ConfigureServices(s => s.AddDbContext<ProductsContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ProductsConnection"))));
+
+            webHostBuilder.UseStartup<Startup>();
+
+            Server = new TestServer(webHostBuilder);
+
+            Client = Server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
