9188b2e62a923781c3b0e979ac1c3c32cc7a81ec
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-05-31 17:02:53 +0200

    added more tests

--- OrdersAPI/Controllers/OrdersController.cs
+++ OrdersAPI/Controllers/OrdersController.cs
@@ -27,13 +27,14 @@ namespace OrdersAPI.Controllers
         {
             return await _context.Orders.Include(c => c.OrderItems).OrderByDescending(c=>c.OrderDate).ToListAsync();
         }
+
         [HttpGet("user/{id}")]
         public async Task<ActionResult<IEnumerable<Order>>> GetUserOrders(string id)
         {
             return await _context.Orders.Include(c => c.OrderItems).OrderByDescending(c => c.OrderDate).Where(c=>c.UserId==id).ToListAsync();
         }
 
-        // GET: api/Orders/5
+        // GET: api/Orders/id
         [HttpGet("{id}")]
         public async Task<ActionResult<Order>> GetOrder(Guid id)
         {
@@ -47,41 +48,37 @@ namespace OrdersAPI.Controllers
             return order;
         }
 
-        // PUT: api/Orders/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutOrder(Guid id, Order order)
-        //{
-        //    if (id != order.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        //PUT: api/Orders/id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(Guid id, Order order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
 
-        //    _context.Entry(order).State = EntityState.Modified;
+            _context.Entry(order).State = EntityState.Modified;
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!OrderExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-        //    return Ok();
-        //}
+            return Ok(order);
+        }
 
         // POST: api/Orders
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<Order>> PostOrder(Order order)
         {
@@ -91,7 +88,7 @@ namespace OrdersAPI.Controllers
             return CreatedAtAction("GetOrder", new { id = order.Id }, order);
         }
 
-        // DELETE: api/Orders/5
+        // DELETE: api/Orders/id
         [HttpDelete("{id}")]
         public async Task<ActionResult<Order>> DeleteOrder(Guid id)
         {
@@ -104,7 +101,7 @@ namespace OrdersAPI.Controllers
             _context.Orders.Remove(order);
             await _context.SaveChangesAsync();
 
-            return order;
+            return Ok(id);
         }
 
         private bool OrderExists(Guid id)
